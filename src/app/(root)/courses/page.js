35d5550369ef13c0891bf9822f74284489488696
bilalattari <attari1235@gmail.com>
2024-10-15 10:39:09 +0500

    course page commented

--- src/app/(root)/courses/page.js
+++ src/app/(root)/courses/page.js
@@ -2,16 +2,16 @@ import CourseCard from "@/components/courseCard";
 import AddCourseForm from "@/components/courseForm";
 
 const Courses = async () => {
-  let courses = await fetch(`${process.env.API_URL}api/courses`, {
-    cache: "no-cache",
-  });
-  courses = await courses.json();
+  // let courses = await fetch(`${process.env.API_URL}api/courses`, {
+  //   cache: "no-cache",
+  // });
+  // courses = await courses.json();
   return (
     <div className="p-20">
       <AddCourseForm />
-      {courses?.courses?.map((course) => (
+      {/* {courses?.courses?.map((course) => (
         <CourseCard key={course.id} item={course} />
-      ))}
+      ))} */}
     </div>
   );
 };
